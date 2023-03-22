f2b23da6a09878a82aa1f93101e6e0f6b46dcac5
Artem <artembajdin3396mics@gmail.com>
2023-03-22 22:00:43 +0500

    version 1.1

--- app/src/main/java/com/soviet_wave/youtube_downloader/MainActivity.java
+++ app/src/main/java/com/soviet_wave/youtube_downloader/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,7 +23,7 @@ import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.io.IOException;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,9 +32,16 @@ public class MainActivity extends AppCompatActivity {
     private TextView debug;
     private FloatingActionButton settings;
     private ProgressBar pBar;
+    private Button loveb;
+
+
 
     public static final String APP_PREFERENCES = "settings";
     SharedPreferences pref;
+
+    String[] itemView = { "Video", "Only_audio(experimental)" };
+
+    public String item = "Video";
 
 
 
@@ -40,10 +51,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String android_v = (Build.VERSION.RELEASE);
         Toast.makeText(MainActivity.this,"Powered by         Soviet_Wave", Toast.LENGTH_SHORT).show();
         edit_url = findViewById(R.id.editText);
         start_bt = findViewById(R.id.button);
         debug = findViewById(R.id.textV);
+        loveb = findViewById(R.id.button2);
         settings = findViewById(R.id.Settings_button);
         pref = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         pBar = findViewById(R.id.progressBar);
@@ -53,12 +66,42 @@ public class MainActivity extends AppCompatActivity {
             Python.start(new AndroidPlatform(this));
         }
 
+        Spinner spinner = findViewById(R.id.spinner);
+        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, itemView);
+        // Определяем разметку для использования при выборе элемента
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Применяем адаптер к элементу spinner
+        spinner.setAdapter(adapter);
+
         Python py  = Python.getInstance();
 
 
 
 
         PyObject pyobj = py.getModule("YouTube");
+
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                // Получаем выбранный объект
+                item = (String)parent.getItemAtPosition(position);
+                boolean debug_mode = pref.getBoolean("debug_mode",false);
+                if (debug_mode==true){
+                    Toast.makeText(MainActivity.this,item, Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        };
+        spinner.setOnItemSelectedListener(itemSelectedListener);
+
 
 
         start_bt.setOnClickListener(new View.OnClickListener() {
@@ -74,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         String url = edit_url.getText().toString();
                         Toast.makeText(MainActivity.this, getString(R.string.toast_download), Toast.LENGTH_SHORT).show();
-                        PyObject obj = pyobj.callAttr("main", url);
+                        PyObject obj = pyobj.callAttr("main", url, item);
                         Toast.makeText(MainActivity.this, getString(R.string.toast_done), Toast.LENGTH_SHORT).show();
                         boolean debug_mode = pref.getBoolean("debug_mode",false);
                         if (debug_mode==true){
@@ -94,11 +137,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        loveb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://new.donatepay.ru/@884550"));
+                startActivity(browserIntent);
+            }
+        });
+
+
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Settings.class);
                 startActivity(intent);
+
+
 
             }
         });
--- app/src/main/python/YouTube.py
+++ app/src/main/python/YouTube.py
@@ -1,17 +1,29 @@
 import pytube
+import os
 
 
-def main(url):
+def main(url, item):
 
 
     dire = ("/storage/emulated/0/Download")
 
     yt = pytube.YouTube(url)
-    ft = yt.title
-    vid = yt.streams.get_highest_resolution()
+    #ft = yt.title
 
-    va = yt.author
-    vd = yt.description
+    if item=="Video":
+        vid = yt.streams.get_highest_resolution()
+        vid.download(dire)
+    else:
+        vid = yt.streams.filter(only_audio=True).desc().first()
+        out_file = vid.download(dire)
+        # save the file
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
 
-    vid.download(dire)
-    return "(Debug massage)Ну, наверное скачалось"
+
+
+    #va = yt.author
+    #vd = yt.description
+
+    #vid.download(dire)
