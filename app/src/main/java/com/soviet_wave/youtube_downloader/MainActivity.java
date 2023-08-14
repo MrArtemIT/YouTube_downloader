fd6338337691f3d75dcbb97d3af5f9fd8202805a
Artem <artembajdin3396mics@gmail.com>
2023-08-14 22:58:14 +0500

    version 1.6 - Обновлен код библиотеки скачивания - Проект выведен в open source и добавлена кнопка перехода на github в разделе настроек

--- app/src/main/java/com/soviet_wave/youtube_downloader/MainActivity.java
+++ app/src/main/java/com/soviet_wave/youtube_downloader/MainActivity.java
@@ -1,14 +1,23 @@
 package com.soviet_wave.youtube_downloader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.os.Vibrator;
 import android.util.Log;
+import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,8 +32,6 @@ import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-
 
 
 public class MainActivity extends AppCompatActivity {
@@ -45,8 +52,29 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences pref;
 
 
+    // Идентификатор уведомления
+    private static final int NOTIFY_ID = 101;
 
+    // Идентификатор канала
+    private static String CHANNEL_ID = "Cat channel";
+    //Длина вибрации (пауза, вибрация, пауза и тд)
+    public long[] pattern = {0, 100, 100, 100, 100, 100};
 
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         pBar.setVisibility(View.INVISIBLE);
         pBar_test.setVisibility(View.INVISIBLE);
         debug.setText("");
-        if (!Python.isStarted()){
+        if (!Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
         }
 
@@ -74,7 +102,10 @@ public class MainActivity extends AppCompatActivity {
         //String item_only_audio = getString(R.string.only_audio);
         //String item_playlist = getString(R.string.playlist);
         //String[] itemView = { item_video, item_only_audio, item_playlist };
-        String[] itemView = { "Video", "Playlist(BETA)", "Audio" };
+
+        //Это список объектов в выпадающем списке
+        String[] itemView = {"Video", "Playlist(BETA)", "Audio"};
+
         Spinner spinner = findViewById(R.id.spinner);
         // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
         ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, itemView);
@@ -82,15 +113,15 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Применяем адаптер к элементу spinner
         spinner.setAdapter(adapter);
-        Python py  = Python.getInstance();
-
-
+        Python py = Python.getInstance();
 
 
         PyObject pyobj = py.getModule("YouTube");
         PyObject pyobj_playlist = py.getModule("playlist_download");
         PyObject pyobj_audio = py.getModule("audio_download");
         //Уведомление
+        // Идентификатор уведомления
+
 
         //Выпадающий список
         AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
@@ -98,10 +129,10 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
                 // Получаем выбранный объект
-                item_static = (String)parent.getItemAtPosition(position);
-                boolean debug_mode = pref.getBoolean("debug_mode",false);
-                if (debug_mode==true){
-                    Toast.makeText(MainActivity.this,item_static, Toast.LENGTH_SHORT).show();
+                item_static = (String) parent.getItemAtPosition(position);
+                boolean debug_mode = pref.getBoolean("debug_mode", false);
+                if (debug_mode == true) {
+                    Toast.makeText(MainActivity.this, item_static, Toast.LENGTH_SHORT).show();
                 }
 
 
@@ -116,12 +147,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 debug.setText("");
-                if (edit_url.getText().toString().equals(""))
-                {
-                    Toast.makeText(MainActivity.this,getString(R.string.toast_url), Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
+                start_bt.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                if (edit_url.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, getString(R.string.toast_url), Toast.LENGTH_SHORT).show();
+                } else {
                     try {
                         String url = edit_url.getText().toString();
                         Toast.makeText(MainActivity.this, getString(R.string.toast_download), Toast.LENGTH_SHORT).show();
@@ -137,31 +166,37 @@ public class MainActivity extends AppCompatActivity {
                             public void run() {
                                 try {
                                     Log.d("item_static", item_static);
-                                    if (item_static=="Video") {
+                                    if (item_static == "Video") {
                                         Log.d("Thread", "Video");
                                         PyObject obj = pyobj.callAttr("main", url, item_static);
 
-                                    }else if (item_static=="Playlist(BETA)") {
+                                    } else if (item_static == "Playlist(BETA)") {
                                         Log.d("Thread", "Playlist");
                                         PyObject obj = pyobj_playlist.callAttr("main", url, item_static);
-                                    }else if (item_static=="Audio"){
+                                    } else if (item_static == "Audio") {
                                         Log.d("Thread", "Audio");
                                         PyObject obj = pyobj_audio.callAttr("main", url, item_static);
-                                    //}else if (item_static=="Видео"){
-                                    //    Log.d("Thread", "Видео");
-                                    //    PyObject obj = pyobj.callAttr("main", url, item_static);
-                                    //}else if (item_static=="Плейлист(БЕТА)"){
-                                    //    Log.d("Thread", "Плейлист");
-                                    //    PyObject obj = pyobj_playlist.callAttr("main", url, item_static);
-                                    }else{
+                                        //}else if (item_static=="Видео"){
+                                        //    Log.d("Thread", "Видео");
+                                        //    PyObject obj = pyobj.callAttr("main", url, item_static);
+                                        //}else if (item_static=="Плейлист(БЕТА)"){
+                                        //    Log.d("Thread", "Плейлист");
+                                        //    PyObject obj = pyobj_playlist.callAttr("main", url, item_static);
+                                    } else {
                                         Log.d("Thread", "Аудио");
                                         PyObject obj = pyobj_audio.callAttr("main", url, item_static);
                                     }
+                                    //Запуск вибрации
+                                    Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+                                    vibrator.vibrate(pattern, -1);
+
                                     pBar.setVisibility(View.INVISIBLE);
 
                                 } catch (Exception main) {
                                     Log.d("Thread_ERROR", "Капец пришёл потоку");
                                     pBar.setVisibility(View.INVISIBLE);
+                                    Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+                                    vibrator.vibrate(1000);
                                     debug.setText(getString(R.string.toast_url_err));
                                     try {
                                         Thread.sleep(5000);
@@ -178,10 +213,10 @@ public class MainActivity extends AppCompatActivity {
                         t.start();
 
 
-                    } catch (Exception main){
-                        Toast.makeText(MainActivity.this,getString(R.string.toast_url_err), Toast.LENGTH_SHORT).show();
-                        boolean debug_mode = pref.getBoolean("debug_mode",false);
-                        if (debug_mode==true){
+                    } catch (Exception main) {
+                        //Toast.makeText(MainActivity.this, getString(R.string.toast_url_err), Toast.LENGTH_SHORT).show();
+                        boolean debug_mode = pref.getBoolean("debug_mode", false);
+                        if (debug_mode == true) {
                             debug.setText("(debug massage) Сработал обработчик ошибок");
                         }
                         Log.d("Thread_ERROR", "Капец пришёл потоку");
@@ -196,6 +231,7 @@ public class MainActivity extends AppCompatActivity {
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                settings.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                 Intent intent = new Intent(MainActivity.this, Settings.class);
                 startActivity(intent);
 
@@ -205,8 +241,8 @@ public class MainActivity extends AppCompatActivity {
         tg.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
+                tg.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.telegram_linkV)));
                 startActivity(browserIntent);
             }
@@ -216,9 +252,11 @@ public class MainActivity extends AppCompatActivity {
         love.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                love.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://boosty.to/mr_artem/donate"));
                 startActivity(browserIntent);
             }
+
         });
     }
 }
--- app/src/main/java/com/soviet_wave/youtube_downloader/Settings.java
+++ app/src/main/java/com/soviet_wave/youtube_downloader/Settings.java
@@ -3,15 +3,22 @@ package com.soviet_wave.youtube_downloader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.HapticFeedbackConstants;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
     private Switch debug_sweech;
     private TextView debug_text;
+    private ImageButton button_github;
     private SharedPreferences pref;
     private final String save_key = "save_key";
     public static final String APP_PREFERENCES = "settings";
@@ -25,7 +32,10 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Switch debug_sweech = (Switch) findViewById(R.id.switch_debug);
         TextView debug_text = findViewById(R.id.textView2);
+        button_github = findViewById(R.id.github_Button);
         pref = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+
+        //Получаем значения из памяти чтобы отобразить верное значение переключателя вкл/выкл
         boolean debug_mode = pref.getBoolean("debug_mode",false);
         SharedPreferences.Editor editor = pref.edit();
         if(debug_mode == true){
@@ -35,6 +45,7 @@ public class Settings extends AppCompatActivity {
         }
         debug_text.setText("");
 
+        //Переключатель debug режима
         debug_sweech.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -42,7 +53,7 @@ public class Settings extends AppCompatActivity {
                 // при изменении состояния switch новое значение
                 //будет сразу записано в соответствующее "поле" в Ваших SharedPreferences
 
-
+                debug_sweech.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                     if (debug_mode == false) {
                         //debug_text.setText("debug_mode is on");
                         editor.putBoolean("debug_mode", true);
@@ -54,6 +65,16 @@ public class Settings extends AppCompatActivity {
                         editor.apply();
                     }
 
+            }
+        });
+
+        //Кнопка для перехода на github
+        button_github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button_github.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/MrArtemIT/YouTube_downloader"));
+                startActivity(browserIntent);
             }
         });
     }
